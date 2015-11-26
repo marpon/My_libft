4b1b92f092c887e166a0bb7e7e7b8037675bd4ae
marpon <marpon@users.noreply.github.com>
2015-11-26 17:29:07 +0100

    Create ft_lstdel.c

---
+++ ft_lstdel.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*list;
+
+	if (!alst || !del)
+		return ;
+	list = *alst;
+	while (list)
+	{
+		del(list->content, list->content_size);
+		free(list);
+		list = list->next;
+	}
+	*alst = NULL;
+}
