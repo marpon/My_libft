39e133a6530ff02ea6e06549033d61f292618fcf
marpon <marpon@users.noreply.github.com>
2015-11-26 16:38:32 +0100

    Create ft_strsub.c

---
+++ ft_strsub.c
@@ -0,0 +1,25 @@
+
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t		length;
+	char		*out;
+
+	if (s == NULL)
+		return (NULL);
+	length = ft_strlen(s);
+	out = NULL;
+	if ((start + len) <= length)
+	{
+		out = (char *)malloc(sizeof(char) * (len + 1));
+		if (out)
+		{
+			out = ft_strncpy(out, s + start, len);
+			out[len] = '\0';
+		}
+	}
+	return (out);
+}
