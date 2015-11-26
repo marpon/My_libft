1aec7ff76f271f814d7bf2c99e44709656a2ce48
marpon <marpon@users.noreply.github.com>
2015-11-26 17:31:42 +0100

    Create ft_lstiter.c

---
+++ ft_lstiter.c
@@ -0,0 +1,18 @@
+
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (f)(t_list *elem))
+{
+	t_list *list;
+
+	if (lst)
+	{
+		list = lst;
+		while (list)
+		{
+			f(list);
+			list = list->next;
+		}
+	}
+}
