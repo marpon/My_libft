01b06d3c81f1c89306b8034aa2c8add9afbf11d6
marpon <marpon@users.noreply.github.com>
2015-11-26 16:43:01 +0100

    Create ft_strsplit.c

---
+++ ft_strsplit.c
@@ -0,0 +1,47 @@
+
+
+#include "libft.h"
+#include <stdlib.h>
+
+static int	count_words(char *s, char c)
+{
+	int		words;
+
+	while (*s && *s == c)
+		++s;
+	words = (*s) ? 1 : 0;
+	while (*s)
+	{
+		if (*s == c && s[1] && s[1] != c)
+			++words;
+		++s;
+	}
+	return (words);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	int		words;
+	char	*start;
+	char	**result;
+
+	words = count_words((char *)s, c);
+	if (!s || !c)
+		return (NULL);
+	result = (char **)malloc(sizeof(char *) * (count_words((char *)s, c) + 1));
+	start = (char *)s;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			if (start != s)
+				*(result++) = ft_strsub(start, 0, s - start);
+			start = (char *)s + 1;
+		}
+		++s;
+	}
+	if (start != s)
+		*(result++) = ft_strsub(start, 0, s - start);
+	*result = NULL;
+	return (result - words);
+}
