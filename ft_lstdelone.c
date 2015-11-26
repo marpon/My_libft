f1c4c08308f09b04da1a009f30bffd66630e591f
marpon <marpon@users.noreply.github.com>
2015-11-26 17:28:11 +0100

    Create ft_lstdelone.c

---
+++ ft_lstdelone.c
@@ -0,0 +1,13 @@
+
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (!alst || !del)
+		return ;
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
+}
